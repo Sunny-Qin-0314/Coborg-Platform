c7b0c33d1d668d8acf2c2efbe68f5c18402f0e08
Jason Xiang <jason.xi.xiang@gmail.com>
2021-02-20 17:14:21 -0500

    updated main.ino file

--- arduino_ws/main/main.ino
+++ arduino_ws/main/main.ino
@@ -9,47 +9,47 @@
 ////////////////////////
 
 // State-Switching Button declarations
-int Button0 = 3;  // Button 0 connected to digital pin 2
+const int state_button_pin = 3;  // Button 0 connected to digital pin 2
 int state = 0;    // Initialize state as 0
-unsigned long B0Press = -400;  // The last time button 0 was pressed
-unsigned long debounceDelay = 500;    // The debounce time
+unsigned long state_button_press = -400;  // The last time button 0 was pressed
+unsigned long debounce_delay = 500;    // The debounce time
 
 
 // Sensor Declarations
 // Potentiometer declarations
-#define WINDOW_SIZE_Pot 10
-int POTENTIOMETER_PIN = A0;
-int readingsPot[WINDOW_SIZE_Pot];
-int sumPot = 0;
-int indexPot = 0;
+#define pot_window_size 10
+const int pot_pin = A0;
+int pot_readings[pot_window_size];
+int pot_sum = 0;
+int pot_index = 0;
 // Flex Sensor declarations
-#define WINDOW_SIZE_Flex 10
-int flexPin = A1;
-int readingsFlex[WINDOW_SIZE_Flex];
-int sumFlex = 0;
-int indexFlex = 0;
+#define flex_window_size 10
+const int flex_pin = A1;
+int flex_readings[flex_window_size];
+int flex_sum = 0;
+int flex_index = 0;
 // IR Sensor declarations
-#define WINDOW_SIZE_IR 10
-int IRpin = A5;
-int readingsIR[WINDOW_SIZE_IR];
-int sumIR = 0;
-int indexIR = 0;
+#define ir_window_size 10
+int ir_pin = A5;
+int ir_readings[ir_window_size];
+int ir_sum = 0;
+int ir_index = 0;
 // Ultrasonic Sensor declarations
-int triggerPin = 13;
-int echoPin = 12;
-#define WINDOW_SIZE_Ultra 10
-int readingsUltra[WINDOW_SIZE_Ultra];
-int sumUltra = 0;
-int indexUltra = 0;
+int ultra_trigger_pin = 13;
+int ultra_echo_pin = 12;
+#define ultra_window_size 10
+int ultra_readings[ultra_window_size];
+int ultra_sum = 0;
+int ultra_index = 0;
 
 // Motor Declarations
 // Servo Motor declarations
-const int SERVO_PIN = 8;
+const int servo_pin = 8;
 Servo servo;
 // DC Motor Encoder declarations
 #define enable_motor 4
-#define motorpin1 5
-#define motorpin2 6
+#define dc_encoder_a_pin 5
+#define dc_encoder_b_pin 6
 int encoder_a_value = 0; // Initialize digital record of encoder_a
 int count = 0;
 float angle = 0;
@@ -71,19 +71,19 @@ PID myPID(&Input, &Output, &Setpoint, Kp, Kd, Ki, DIRECT);
 void setup(){
     Serial.begin(9600);
     // State Button attachments
-    pinMode(Button0, INPUT);    // Set Button 0 to an input
-    attachInterrupt(digitalPinToInterrupt(Button0), State_Change, RISING);  // Attach interrupt to Button 0
+    pinMode(state_button_pin, INPUT);    // Set Button 0 to an input
+    attachInterrupt(digitalPinToInterrupt(state_button_pin), State_Change, RISING);  // Attach interrupt to Button 0
     // Ultrasonic Sensor attachments
-    pinMode(triggerPin, OUTPUT);
-    pinMode(echoPin, INPUT);
+    pinMode(ultra_trigger_pin, OUTPUT);
+    pinMode(ultra_echo_pin, INPUT);
     // Servo Motor attachments
-    pinMode(SERVO_PIN,OUTPUT);
-    servo.attach(SERVO_PIN);
+    pinMode(servo_pin,OUTPUT);
+    servo.attach(servo_pin);
     // DC Motor attachments
     pinMode(encoder_a, INPUT);    // Set encoder_a to an input
     attachInterrupt(digitalPinToInterrupt(encoder_a), Increment, FALLING);  // Attach interrupt to encoder_a
-    pinMode(motorpin1, OUTPUT);
-    pinMode(motorpin2, OUTPUT);
+    pinMode(dc_encoder_a_pin, OUTPUT);
+    pinMode(dc_encoder_b_pin, OUTPUT);
     pinMode(enable_motor, OUTPUT);
     myPID.SetOutputLimits(-255,255);
     myPID.SetMode(AUTOMATIC);
@@ -98,27 +98,27 @@ void loop(){
     if (state == 0){
         //Sensor Mode code
         //Potentiometer
-        int analogValuePot = analogRead(POTENTIOMETER_PIN);
+        int analogValuePot = analogRead(pot_pin);
         int avgValuePot= avgFilterPot(analogValuePot);
         float anglePot = map(avgValuePot, 0, 1024, 0, 180.0);
         // Flex sensor  (resistance VS force)
         // https://www.tinkercad.com/things/g7jKPpMlgkY-powerful-blorr-gaaris/editel 
         // All the flex sensor + servo motor code is in the above simulation circuit
-        int val = analogRead(flexPin);  // 707-880 -> 3.45V-4.29V (0-1023 -> 0-5V) . R = 10k . Rx = 22k to 60k
+        int val = analogRead(flex_pin);  // 707-880 -> 3.45V-4.29V (0-1023 -> 0-5V) . R = 10k . Rx = 22k to 60k
         int avg = avgFilterFlex(val);
         float angleFlex = map(avg, 700, 860, 0, 180.0);
         //IR Sensor
-        int reading = analogRead(IRpin);
+        int reading = analogRead(ir_pin);
         int average = avgFilterIR(reading);
         float voltage = (average/1024.0)*5;
         float dist = 50.6/(voltage-0.173);   // cm
         // Ultrasonic Sensor
-        digitalWrite(triggerPin, LOW);  
+        digitalWrite(ultra_trigger_pin, LOW);  
         delayMicroseconds(2);
-        digitalWrite(triggerPin, HIGH);
+        digitalWrite(ultra_trigger_pin, HIGH);
         delayMicroseconds(10); // Hold trigger for 10 microseconds, which is signal for sensor to measure distance.
-        digitalWrite(triggerPin, LOW);
-        unsigned long durationMicroSec = pulseIn(echoPin, HIGH);
+        digitalWrite(ultra_trigger_pin, LOW);
+        unsigned long durationMicroSec = pulseIn(ultra_echo_pin, HIGH);
         int avgDurationMicroSec = avgFilterUltra((int) durationMicroSec);
         double distanceCm = avgDurationMicroSec / 2.0 * 0.0340;  // 340m/s  0.0340cm/microsec
         if(distanceCm > 100.0){
@@ -140,13 +140,13 @@ void loop(){
         Serial.print(Input); Serial.print("\t"); Serial.print(Setpoint); Serial.print("\t"); Serial.println(Output);
 
         if(Output > 0){//CounterClockwise
-            digitalWrite(motorpin1,HIGH);
-            digitalWrite(motorpin2,LOW);
+            digitalWrite(dc_encoder_a_pin,HIGH);
+            digitalWrite(dc_encoder_b_pin,LOW);
             analogWrite(enable_motor,Output);
         }
         else if(Output < 0){//Clockwise
-            digitalWrite(motorpin1,LOW);
-            digitalWrite(motorpin2,HIGH);
+            digitalWrite(dc_encoder_a_pin,LOW);
+            digitalWrite(dc_encoder_b_pin,HIGH);
             analogWrite(enable_motor,-Output);
         }
 
@@ -174,11 +174,11 @@ void loop(){
 
 // Function to debounce switch and change state
 void State_Change() {
-  if ((millis() - B0Press) > debounceDelay) { // Check to see when button was last pressed
+  if ((millis() - state_button_press) > debounce_delay) { // Check to see when button was last pressed
     state += 1;   // If Button 0 is pushed, increment the state
     if (state == 3){
       state = 0;    // If currently state 2, cycle back to state 0
-      B0Press = millis(); // Grab the time at which the button was pressed
+      state_button_press = millis(); // Grab the time at which the button was pressed
     }
     Serial.print("State:");
     Serial.println(state);
@@ -187,10 +187,10 @@ void State_Change() {
 
 
 void Increment() {
-  if(digitalRead(motorpin1)==HIGH){ //CounterClockwise
+  if(digitalRead(dc_encoder_a_pin)==HIGH){ //CounterClockwise
     count++;
   }
-  else if(digitalRead(motorpin2)==HIGH){ //Clockwise
+  else if(digitalRead(dc_encoder_b_pin)==HIGH){ //Clockwise
     count--;
   }
 }
@@ -198,42 +198,42 @@ void Increment() {
 
 int avgFilterIR(int reading)
 {
-  sumIR -= readingsIR[indexIR];  // remove the earliest reading
-  readingsIR[indexIR] = reading;  // update the reading array
-  sumIR += readingsIR[indexIR];   // add new reading to the sum
-  indexIR = (indexIR+1) % WINDOW_SIZE_IR;   // move the “index” pointer to the next
-  return sumIR/WINDOW_SIZE_IR;
+  ir_sum -= ir_readings[ir_index];  // remove the earliest reading
+  ir_readings[ir_index] = reading;  // update the reading array
+  ir_sum += ir_readings[ir_index];   // add new reading to the sum
+  ir_index = (ir_index+1) % ir_window_size;   // move the “index” pointer to the next
+  return ir_sum/ir_window_size;
 }
 
 int avgFilterUltra(int reading)
 {
-  //Serial.println(readingsUltra[1]);
-  //Serial.println(indexUltra);
-  sumUltra -= readingsUltra[indexUltra];  // remove the earliest reading
-  readingsUltra[indexUltra] = reading;  // update the reading array
-  sumUltra += readingsUltra[indexUltra];   // add new reading to the sum
-  indexUltra = (indexUltra+1) % WINDOW_SIZE_Ultra;   // move the “index” pointer to the next
-  return sumUltra/WINDOW_SIZE_Ultra;
+  //Serial.println(ultra_readings[1]);
+  //Serial.println(ultra_index);
+  ultra_sum -= ultra_readings[ultra_index];  // remove the earliest reading
+  ultra_readings[ultra_index] = reading;  // update the reading array
+  ultra_sum += ultra_readings[ultra_index];   // add new reading to the sum
+  ultra_index = (ultra_index+1) % ultra_window_size;   // move the “index” pointer to the next
+  return ultra_sum/ultra_window_size;
 }
 
 int avgFilterFlex(int reading)
 {
-  //Serial.println(readingsFlex[1]);
-  //Serial.println(indexFlex);
-  sumFlex -= readingsFlex[indexFlex];  // remove the earliest reading
-  readingsFlex[indexFlex] = reading;  // update the reading array
-  sumFlex += readingsFlex[indexFlex];   // add new reading to the sum
-  indexFlex = (indexFlex+1) % WINDOW_SIZE_Flex;   // move the “index” pointer to the next
-  return sumFlex/WINDOW_SIZE_Flex;
+  //Serial.println(flex_readings[1]);
+  //Serial.println(flex_index);
+  flex_sum -= flex_readings[flex_index];  // remove the earliest reading
+  flex_readings[flex_index] = reading;  // update the reading array
+  flex_sum += flex_readings[flex_index];   // add new reading to the sum
+  flex_index = (flex_index+1) % flex_window_size;   // move the “index” pointer to the next
+  return flex_sum/flex_window_size;
 }
 
 int avgFilterPot(int reading)
 {
-  //Serial.println(readingsPot[1]);
-  //Serial.println(indexPot);
-  sumPot -= readingsPot[indexPot];  // remove the earliest reading
-  readingsPot[indexPot] = reading;  // update the reading array
-  sumPot += readingsPot[indexPot];   // add new reading to the sum
-  indexPot = (indexPot+1) % WINDOW_SIZE_Pot;   // move the “index” pointer to the next
-  return sumPot/WINDOW_SIZE_Pot;
+  //Serial.println(pot_readings[1]);
+  //Serial.println(pot_index);
+  pot_sum -= pot_readings[pot_index];  // remove the earliest reading
+  pot_readings[pot_index] = reading;  // update the reading array
+  pot_sum += pot_readings[pot_index];   // add new reading to the sum
+  pot_index = (pot_index+1) % pot_window_size;   // move the “index” pointer to the next
+  return pot_sum/pot_window_size;
 }
