21fcd4cb340b8950036164504e86256129707d98
husamwadi <husamwadi2@gmail.com>
2021-03-18 10:51:01 -0400

    updated home directory

--- catkin_ws/build/voice_recog/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/coborg/Coborg-Platform/catkin_ws/src/voice_recog
-# Build directory: /home/coborg/Coborg-Platform/catkin_ws/build/voice_recog
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
