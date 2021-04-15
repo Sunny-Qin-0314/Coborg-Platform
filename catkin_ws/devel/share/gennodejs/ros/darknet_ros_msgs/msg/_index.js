2e6fc69bb2abd130cb548ae805c7e0e16bdbbcd5
themorethegerryer <gdascoli@terpmail.umd.edu>
2021-04-15 09:30:34 -0400

    updated catkin_ws vision and voice

--- catkin_ws/devel/share/gennodejs/ros/darknet_ros_msgs/msg/_index.js
+++ catkin_ws/devel/share/gennodejs/ros/darknet_ros_msgs/msg/_index.js
@@ -1,26 +1,26 @@
 
 "use strict";
 
-let BoundingBox = require('./BoundingBox.js');
 let BoundingBoxes = require('./BoundingBoxes.js');
+let BoundingBox = require('./BoundingBox.js');
 let ObjectCount = require('./ObjectCount.js');
-let CheckForObjectsAction = require('./CheckForObjectsAction.js');
 let CheckForObjectsGoal = require('./CheckForObjectsGoal.js');
-let CheckForObjectsResult = require('./CheckForObjectsResult.js');
-let CheckForObjectsFeedback = require('./CheckForObjectsFeedback.js');
-let CheckForObjectsActionFeedback = require('./CheckForObjectsActionFeedback.js');
+let CheckForObjectsAction = require('./CheckForObjectsAction.js');
 let CheckForObjectsActionResult = require('./CheckForObjectsActionResult.js');
+let CheckForObjectsFeedback = require('./CheckForObjectsFeedback.js');
 let CheckForObjectsActionGoal = require('./CheckForObjectsActionGoal.js');
+let CheckForObjectsResult = require('./CheckForObjectsResult.js');
+let CheckForObjectsActionFeedback = require('./CheckForObjectsActionFeedback.js');
 
 module.exports = {
-  BoundingBox: BoundingBox,
   BoundingBoxes: BoundingBoxes,
+  BoundingBox: BoundingBox,
   ObjectCount: ObjectCount,
-  CheckForObjectsAction: CheckForObjectsAction,
   CheckForObjectsGoal: CheckForObjectsGoal,
-  CheckForObjectsResult: CheckForObjectsResult,
-  CheckForObjectsFeedback: CheckForObjectsFeedback,
-  CheckForObjectsActionFeedback: CheckForObjectsActionFeedback,
+  CheckForObjectsAction: CheckForObjectsAction,
   CheckForObjectsActionResult: CheckForObjectsActionResult,
+  CheckForObjectsFeedback: CheckForObjectsFeedback,
   CheckForObjectsActionGoal: CheckForObjectsActionGoal,
+  CheckForObjectsResult: CheckForObjectsResult,
+  CheckForObjectsActionFeedback: CheckForObjectsActionFeedback,
 };
