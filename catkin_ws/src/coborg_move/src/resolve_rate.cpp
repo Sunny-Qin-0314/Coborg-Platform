e92b62910994f7a898b56bc21d53d160662d543c
themorethegerryer <gdascoli@terpmail.umd.edu>
2021-09-27 15:13:05 -0400

    modify resolve rate

--- catkin_ws/src/coborg_move/src/resolve_rate.cpp
+++ catkin_ws/src/coborg_move/src/resolve_rate.cpp
@@ -19,15 +19,16 @@
 #include <algorithm> //based on HEBI C++ API 3.3.0 documentation
 #include <cmath>
 
+#include "std_msgs/Bool.h"
 #include <gb_visual_detection_3d_msgs/goal_msg.h>
 
 tf::StampedTransform* prevTransform;
 tf::Listener* globalListener;
 gb_visual_detection_3d_msgs::goal_msg* goal; 
+bool enable_rr;
 
 geometry_msgs::Pose poseMotionDetection(const geometry_msgs::Pose::ConstPtr& pose_msg)
-{
-    
+{    
     tf::StampedTransform transform;
     geometry_msgs::Pose target_pose;
 
@@ -36,7 +37,7 @@ geometry_msgs::Pose poseMotionDetection(const geometry_msgs::Pose::ConstPtr& pos
         // TODO: acquire the transform once
         // FORNOW: update transform parameters at every callback interval
         //listener.waitForTransform("/world", "/camera_link", ros::Time(0), ros::Duration(3.0));
-        globalListener->lookupTransform("/t265_odom", "/end_link/INPUT_INTERFACE",ros::Time(0), transform);
+        globalListener->lookupTransform("/t265_odom", "/motor1_link/INPUT_INTERFACE",ros::Time(0), transform);
 
         // FORNOW: only goal position is updated b/c 3DoF robot arm cannot solve 6DoF goal every time
         target_pose.position.x = -(transform.getOrigin().getX()-prevTransform->getOrigin().getX()) + pose_msg->position.x;
@@ -55,9 +56,41 @@ geometry_msgs::Pose poseMotionDetection(const geometry_msgs::Pose::ConstPtr& pos
     }
 }
 
-void get_goalpose_callback(const gb_visual_detection_3d_msgs::goal_msg::ConstPtr& goal_msg)
+void goal_callback(const gb_visual_detection_3d_msgs::goal_msg::ConstPtr& goal_msg)
 {
-    goal = goal_msg;
+    tf::StampedTransform transform;
+
+    bool goal_got = false
+    while(!goal_got)
+    {
+        try
+        {
+            // TODO: acquire the transform once
+            // FORNOW: update transform parameters at every callback interval
+            //listener.waitForTransform("/world", "/camera_link", ros::Time(0), ros::Duration(3.0));
+            globalListener->waitForTransform("/d400_link", "/t265_odom", ros::Time(0), ros::Duration(3.0), transform);
+
+            // FORNOW: only goal position is updated b/c 3DoF robot arm cannot solve 6DoF goal every time
+            goal.position.x = -transform.getOrigin().getX() + goal_msg->position.x;
+            goal.position.y = -transform.getOrigin().getY() + goal_msg->position.y;
+            goal.position.z = -transform.getOrigin().getZ() + goal_msg->position.z;
+
+            ROS_INFO("Transforms are: x: %f, y: %f: z: %f", goal.position.x,goal.position.y, goal.position.z);
+
+            // robot arm can now move to updated goal pose
+            goal_got = true;
+        }
+        catch (tf::TransformException &ex)
+        {
+            ROS_ERROR("%s", ex.what());
+            goal_got = false;
+        }
+    }
+}
+
+void enable_rr_callback(const std_msgs::Bool::ConstPtr& bool_msg)
+{
+    enable_rr = bool_msg->data;
 }
 
 int main(int argc, char **argv)
@@ -65,15 +98,94 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "resolved_rate_planner");
     ros::NodeHandle node;
 
+    // Establish initial values
     tf::TransformListener listener;
-    globalListener = &listener
+    globalListener = &listener;
 
-    // Establish initial tf
     tf::Transform tempTrans;
     globalListener->waitForTransform("/t265_odom", "/end_link/INPUT_INTERFACE", ros::Time(0), ros::Duration(3.0), tempTrans);
     prevTransform = &tempTrans;
 
-    ros::Subscriber goal_pos_sub = node.subscribe("/goal",1,get_goalpose_callback);
-    
+    enable_rr = false;
 
+    // Get goal position once it's published by vision nodes (same goal move_it picks up)
+    ros::Subscriber goal_pos_sub = node.subscribe("/goal",1,goal_callback);
+    // Topic to trigger resolved rate to begin
+    ros::Subscriber tigger_rr_sub = node.subscribe("/enable_rr",1,enable_rr_callback);
+
+
+    // inititalize HEBI API
+    std::vector<std::string> families;
+    families = {"X5-9","X5-9","X5-4"};
+    std::vector<std::string> names;
+    names = {"base_1", "elbow_2", "wrist_3"};
+
+    // connect to HEBI joints on network through UDP connection
+    std::shared_ptr<hebi::Group> group;
+    for (int num_tries = 0; num_tries < 3; num_tries++) {
+      hebi::Lookup lookup;
+      group = lookup.getGroupFromNames(families, names);
+      if (group) {
+        //print hebi modules to terminal
+        auto entry_list = lookup.getEntryList();
+
+        for(size_t hebi_iter = 0; hebi_iter < entry_list->size(); ++ hebi_iter)
+        {
+            std::cout << (*entry_list)[hebi_iter].family_ << " | " << (*entry_list)[hebi_iter].name_ << std::endl;
+        }
+        break;
+      }
+      ROS_WARN("Could not find group actuators, trying again...");
+      ros::Duration(1.0).sleep();
+    }
+    //code stolen from hebi_cpp_api_examples/src/basic/group_node.cpp
+
+    // error out if HEBI joints are not found on the network
+    if (!group) {
+      ROS_ERROR("Could not initialize arm! Check for modules on the network, and ensure good connection (e.g., check packet loss plot in Scope). Shutting down...");
+      return -1;
+    }
+    //print names of each HEBI item in the group
+    std::cout << "Total Number of HEBI Members in group: " << group->size() << std::endl;
+
+    hebi::GroupCommand groupCommand(group->size());
+
+    hebi::GroupFeedback group_feedback(group->size());
+    Eigen::VectorXd thetas(group->size());
+
+    float dt = 0.01;
+    Eigen::Identity W(3,3);
+
+    ros::Rate rate(20.0);
+    while(ros::ok())
+    {
+        if(enable_rr)
+        {
+            //xg = goal
+            gb_visual_detection_3d_msgs::goal_msg xg = *goal;
+
+            //theta = Get joint state
+            if (group->getNextFeedback(group_feedback))
+            {
+                thetas = group_feedback.getPosition(); 
+            }
+            
+            //x0 = forward_kinematics(theta)
+            //[x,y,z of the end effector]
+
+            //J = compute_jacobian(theta)
+            //[2d matrix of joint angles ]
+
+            //thetadot = inv(W)*J.T*inv(J*inv(W)*J.T)*(xg-x0)
+            
+            //theta = theta + dt*thetadot
+            
+            //command_angles(theta)
+            groupCommand.setPosition(thetas);
+            group->sendCommand(groupCommand);
+
+        }
+        ros::spinOnce();
+        rate.sleep();
+    }
 }
